52592f3b4649df7452f14c4b03adb2c343a8e8eb
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-10 10:44:27 +0530

    Added: Percentage of Heads & Tails Logic

--- src/com/blz/FlipCoin1/FlipCoin1.java
+++ src/com/blz/FlipCoin1/FlipCoin1.java
@@ -1,10 +1,286 @@
 package com.blz.FlipCoin1;
-
+import java.util.Scanner;
 public class FlipCoin1 {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to FlipCoin Program!!");
-
+		System.out.println("Welcome to FlipCoin1 Program!!");
+		
+		 int heads = 0;
+		 int tails = 0;
+		 int counts = 1;
+		 double random = 0.0;
+		 
+		 System.out.println("Enter the number:");
+		 Scanner n = new Scanner(System.in);
+		 int flips = n.nextInt();
+		 
+		 while(counts <= flips) {
+			 random = Math.random();
+			 System.out.println(counts+" "+random);
+			 
+			 if(random < 0.5) {
+				 heads++;
+			System.out.println("heads");
+			                  }
+			 
+			 else {
+				 tails++;
+			 System.out.println("tails");
+		          }
+		         counts++;
+		              
+		                       }
+	    
+	     System.out.println("Number of Heads: "+heads);
+	     System.out.println("Number of Tails: "+tails);
+	     
+	     double headPercentage = (double)heads/flips * 100;
+	     double tailPercentage = (double)tails/flips * 100;
+	     
+	     System.out.println("Percentage of Heads: "+headPercentage+"%");
+	     System.out.println("Percentage if Tails: "+tailPercentage+"%");
+	     
 	}
-
+	
 }
+	     
+
+
+
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+		 
+		 
+		 
+		 
+		 
+				 
+				 
+			 
+			 
+			 
+			 
+			 
+			 
+		 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	
+
+
