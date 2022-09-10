1dfef6372142d6b13a5f156a11f61fd7d9f01fa9
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-10 10:16:47 +0530

    Added: Welcome message

---
+++ Readme.md
@@ -0,0 +1,2 @@
+This is a FlipCoin1 Program
+
---
+++ src/com/blz/FlipCoin1/FlipCoin1.java
@@ -0,0 +1,10 @@
+package com.blz.FlipCoin1;
+
+public class FlipCoin1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to FlipCoin Program!!");
+
+	}
+
+}
